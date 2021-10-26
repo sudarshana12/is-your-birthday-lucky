9540156479a51a16860246e779571e56bcdb2820
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-26 20:46:50 +0530

    update

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const luckyNumber = document.querySelector("#lucky-number");
 
 const checkNumberButton = document.querySelector("#check-number");
 
-checkNumberButton.addEventListener('click', function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value );
-})
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum=0;
+    for(let index=0;index<dob.length;index++){
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
